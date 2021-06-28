c77d7e27f9b1260a4ee1ec498990477795c67588
2044smile <2044smile@naver.com>
2021-06-28 21:46:00 +0900

    djust Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def hello_world(request):
-    return HttpResponse('안녕하세요')
+def hello_world(requset):
+    return render(requset, 'base.html')
